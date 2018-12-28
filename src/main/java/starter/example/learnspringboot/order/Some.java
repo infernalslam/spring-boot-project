86373990fb55d54fcd8ae2e3af4d71b705c0fcff
kamonpob.p <kamonpob.p@linecorp.com>
2018-12-28 12:12:29 +0700

    init :tada: controller with services

---
+++ src/main/java/starter/example/learnspringboot/order/Some.java
@@ -0,0 +1,4 @@
+package starter.example.learnspringboot.order;
+
+public class Some {
+}
---
+++ src/main/java/starter/example/learnspringboot/order/services/OrderService.java
@@ -0,0 +1,10 @@
+package starter.example.learnspringboot.order.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    public String heathCheck () {
+        return "welcome spring boot";
+    }
+}
